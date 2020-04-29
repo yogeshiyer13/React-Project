4bec8180e7fb189da2f9e44e4fda67862c25d113
Yogesh Iyer <yogeshiyer13@gmail.com>
2020-04-28 22:30:24 -0400

    On branch master

---
+++ UserInput/UserInput.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const userInput = () => {
+    return <input type= "text" />;
+  
+};
+
+export default userInput;
---
+++ UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>Some Random text!</p>
+            <p>I hope I'll be overwritten</p>
+        </div>
+    );
+};
+
+export default userOutput;
