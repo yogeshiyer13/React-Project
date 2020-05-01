9bb211730caa7624c6b0b32a5af33aceaf27a744
Yogesh Iyer <yogeshiyer13@gmail.com>
2020-05-01 14:21:21 -0400

    Updated upto deleting the contents

--- App.js
+++ App.js
@@ -2,90 +2,83 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-// const app = props => {
-//   const [personsState, setPersonsState] = useState({
-//     persons: [
-//       {name:'Max', age: 28},
-//       {name:'Manu', age: 29},
-//       {name:'Stephanie', age:26}
-
-//     ]
-//   });
-
-// const [otherState, setOtherState] = useState('some other value');
-
-// console.log(personsState, otherState)
-
 class App extends Component {
   state = {
     persons: [
-      {name:'Max', age: 28},
-      {name:'Manu', age: 29},
-      {name:'Stephanie', age:26}
+      {id:'abcd', name:'Max', age: 28},
+      {id:'efgh', name:'Manu', age: 29},
+      {id:'ijkl', name:'Stephanie', age:26}
 
     ],
-    otherState: 'some other value'
-  };
-//(property) App.switchNameHandler: () => void
-switchNameHandler = (newName) => {
-  //console.log('Was clicked');
-  //DONT DO this.state.persons[0].name = 'Yogesh Iyer';
-  // setPersonsState({
-  //   persons: [
-  //     {name:'Yogesh', age: 28},
-  //     {name:'Manu', age: 29},
-  //     {name:'Stephanie', age:27}
-  //   ]
-  // })
+    otherState: 'some other value',
+    showPersons: false
+  }
 
-   this.setState({persons: [
-    {name: newName, age: 28},
-    {name:'Manu', age: 29},
-    {name:'Stephanie', age:27}
 
-  ]
-}
-)
+nameChangedHandler = (event, id) => {
+  const personIndex = this.state.person.findIndex(p => {
+    return p.id === id;
+  });
+
+const person = {
+  ...this.state.persons[personIndex]
+};
+
+person.name = event.target.value;
+
+const persons = [...this.state.persons];
+persons[personIndex] = person;
+
+  this.setState({persons: persons});
 }
 
-nameChangedHandler = (event) => {
-  this.setState({persons: [
-    {name: 'Max', age: 28},
-    {name: event.target.value, age: 29},
-    {name:'Stephanie', age:26}
 
-  ]
-}
-)
+deletePersonHandler = (personIndex) => {        
+  //const persons = this.state.persons.slice();
+  const persons = [...this.state.persons];
+  persons.splice(personIndex, 1);
+  this.setState({persons: persons});
 }
 
+togglePersonsHandler = () => {
+  const doesShow = this.state.showPersons;
+  this.setState({showPersons: !doesShow});
+}
   render() {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
-      border: '1px solidn blue',
+      border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer' 
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {  //Output content conditionally and same for list using JS
+      persons = (
+      <div>
+        {this.state.persons.map((person, index) => {  //Map operatot
+          return <Person 
+          click = {() => this.deletePersonHandler(index)}
+          name={person.name} 
+          age={person.age}
+          key = {person.id} 
+          changed = {(event) => this.nameChangedHandler(event, person.id)} />
+        })}       
+      </div> 
+      );
+
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
         <button
          style={style}
-         onClick={this.switchNameHandler.bind(this, 'Yogesh')}>Switch Name</button>
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}/>
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.switchNameHandler.bind(this, 'Yogu!!')} 
-        changed={this.nameChangedHandler} >My Hobbies: Racing</Person> 
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}/>
+         onClick={() => this.togglePersonsHandler()}>Toggle Persons</button>
+
+        {persons}
       </div>
     );
    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
