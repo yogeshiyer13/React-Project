fb5ae315e4d878e95fdaf436180b1bf71a877b4e
Yogesh Iyer <yogeshiyer13@gmail.com>
2020-05-24 22:32:25 -0400

    More Styling

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled from 'styled-components';
+//import styled from 'styled-components';
 //import Radium,{ StyleRoot } from 'radium';
 import Person from './Person/Person';
 
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red': 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: lightgreen;
-    color:black;
-  }
-  `;
 
 class App extends Component {
   state = {
@@ -112,9 +98,9 @@ if (this.state.persons.length <=1) {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className ={classes.join(' ')}>This is really working!!</p>
-        <StyledButton alt={this.state.showPersons} onClick={() => this.togglePersonsHandler()}>
+        <button className = "button" onClick={() => this.togglePersonsHandler()}>
           Toggle Persons
-        </StyledButton>
+        </button>
 
         {persons}
       </div>
